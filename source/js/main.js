54945eaf7f2c306b25e57726031d2a0cb062b87d
Grigoriy Kozheletov <misteryrus@gmail.com>
2021-12-23 16:14:35 +0300

    add section about

--- source/js/main.js
+++ source/js/main.js
@@ -3,9 +3,19 @@
 window.addEventListener('load', () => {
   const menuButton = document.querySelector('.js-menu-button');
   const menuNav = document.querySelector('.js-site-nav');
+  const readMoreButton = document.querySelector('.js-read-more');
+  const readMoreDotters = document.querySelector('.js-about__dotters');
+  const aboutFullText = document.querySelector('.js-full-text');
 
   menuButton.addEventListener('click', (event) => {
     event.preventDefault();
     menuNav.classList.toggle('menu-hide');
+  });
+
+  readMoreButton.addEventListener('click', (event) => {
+    event.preventDefault();
+    readMoreDotters.classList.toggle('hide');
+    aboutFullText.classList.toggle('show-inline');
+    readMoreButton.textContent = readMoreButton.textContent === 'Читать далее' ? 'Свернуть' : 'Читать далее';
   });
 });
